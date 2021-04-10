14803a951ea8ec0691d6840dd72620e0033a3bca
benny <bennydrm16@gmail.com>
2021-04-10 10:19:42 +0700

    add connect to new user with socket.io

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const server = require('http').createServer(app)
 const { v4: uuidv4 } = require('uuid')
+const io = require('socket.io')(server)
 const port = 3000
 
 app.set('view engine', 'ejs')
@@ -14,6 +16,13 @@ app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
-app.listen(port, () => {
+io.on('connection', socket => {
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId)
+    socket.broadcast.emit('user-connected')
+  })
+})
+
+server.listen(port, () => {
   console.log(`running at port ${port}`)
 })
